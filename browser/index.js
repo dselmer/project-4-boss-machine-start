1945b4bdcaf7f94c89e7dc9f5feef61ce5dfc7db
dselmer <dselmer@icstars.org>
2024-07-21 13:02:55 -0500

    changed port to 3000

--- browser/index.js
+++ browser/index.js
@@ -1,97 +1,106 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import axios from 'axios';
+import ReactDOM from "react-dom";
+import React from "react";
+import { Provider } from "react-redux";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import axios from "axios";
 
-import store from './store/index';
+import store from "./store/index";
 
-import { setMinions } from './store/minions';
-import { setSelectedMinion } from './store/selectedMinion';
-import { setIdeas } from './store/ideas';
-import { setSelectedIdea } from './store/selectedIdea';
-import { setWork } from './store/work';
-import { setMeetings } from './store/meetings'; 
-import { setIdeaEditing, setMinionEditing, resetEditingState } from './store/appState';
+import { setMinions } from "./store/minions";
+import { setSelectedMinion } from "./store/selectedMinion";
+import { setIdeas } from "./store/ideas";
+import { setSelectedIdea } from "./store/selectedIdea";
+import { setWork } from "./store/work";
+import { setMeetings } from "./store/meetings";
+import {
+  setIdeaEditing,
+  setMinionEditing,
+  resetEditingState,
+} from "./store/appState";
 
-import App from './components/App';
-import AllMinions from './components/AllMinions';
-import Home from './components/Home';
-import AllIdeas from './components/AllIdeas';
-import Idea from './components/Idea';
-import Minion from './components/Minion';
+import App from "./components/App";
+import AllMinions from "./components/AllMinions";
+import Home from "./components/Home";
+import AllIdeas from "./components/AllIdeas";
+import Idea from "./components/Idea";
+import Minion from "./components/Minion";
 
-const appEnter = nextRouterState => {
+const appEnter = (nextRouterState) => {
   Promise.all([
-    axios.get('http://localhost:4001/api/minions'),
-    axios.get('http://localhost:4001/api/ideas'),
-    axios.get('http://localhost:4001/api/meetings'),
+    axios.get("http://localhost:3000/api/minions"),
+    axios.get("http://localhost:3000/api/ideas"),
+    axios.get("http://localhost:3000/api/meetings"),
   ])
-  .then(([minionsResponse,ideasResponse, meetingsResponse]) => {
-    return [minionsResponse.data, ideasResponse.data, meetingsResponse.data];
-  })
-  .then(([minions, ideas, meetings]) => {
-    store.dispatch(setMinions(minions));
-    store.dispatch(setIdeas(ideas));
-    store.dispatch(setMeetings(meetings));
-  })
-  .catch(console.error.bind(console));
-}
+    .then(([minionsResponse, ideasResponse, meetingsResponse]) => {
+      return [minionsResponse.data, ideasResponse.data, meetingsResponse.data];
+    })
+    .then(([minions, ideas, meetings]) => {
+      store.dispatch(setMinions(minions));
+      store.dispatch(setIdeas(ideas));
+      store.dispatch(setMeetings(meetings));
+    })
+    .catch(console.error.bind(console));
+};
 
-const singleMinionEnter = nextRouterState => {
+const singleMinionEnter = (nextRouterState) => {
   store.dispatch(resetEditingState());
   const id = nextRouterState.params.id;
-  axios.get(`http://localhost:4001/api/minions/${id}`)
-  .then(res => res.data)
-  .then(minion => {
-    store.dispatch(setSelectedMinion(minion));
-  })
-  .catch(console.error.bind(console));
+  axios
+    .get(`http://localhost:3000/api/minions/${id}`)
+    .then((res) => res.data)
+    .then((minion) => {
+      store.dispatch(setSelectedMinion(minion));
+    })
+    .catch(console.error.bind(console));
 
-  axios.get(`http://localhost:4001/api/minions/${id}/work`)
-  .then(res => res.data)
-  .then(work => {
-    store.dispatch(setWork(work));
-  })
-  .catch(console.error.bind(console));
-}
+  axios
+    .get(`http://localhost:3000/api/minions/${id}/work`)
+    .then((res) => res.data)
+    .then((work) => {
+      store.dispatch(setWork(work));
+    })
+    .catch(console.error.bind(console));
+};
 
-const singleIdeaEnter = nextRouterState => {
+const singleIdeaEnter = (nextRouterState) => {
   const id = nextRouterState.params.id;
-  axios.get(`http://localhost:4001/api/ideas/${id}`)
-  .then(res => res.data)
-  .then(idea => {
-    store.dispatch(setSelectedIdea(idea));
-  })
-  .catch(console.error.bind(console));
-}
+  axios
+    .get(`http://localhost:3000/api/ideas/${id}`)
+    .then((res) => res.data)
+    .then((idea) => {
+      store.dispatch(setSelectedIdea(idea));
+    })
+    .catch(console.error.bind(console));
+};
 
 const newIdeaEnter = () => {
   store.dispatch(setIdeaEditing());
-  store.dispatch(setSelectedIdea({
-      name: 'New Idea',
-      description: '',
+  store.dispatch(
+    setSelectedIdea({
+      name: "New Idea",
+      description: "",
       weeklyRevenue: 0,
       numWeeks: 0,
-    }
-  ))
-}
+    })
+  );
+};
 
 const newMinionEnter = () => {
   store.dispatch(setWork([]));
   store.dispatch(setMinionEditing());
-  store.dispatch(setSelectedMinion({
-      name: '',
-      title: '',
-      weaknesses: '',
+  store.dispatch(
+    setSelectedMinion({
+      name: "",
+      title: "",
+      weaknesses: "",
       salary: 0,
-    }
-  ));
-}
+    })
+  );
+};
 
 const allIdeasEnter = () => {
-  store.dispatch(resetEditingState())
-}
+  store.dispatch(resetEditingState());
+};
 
 ReactDOM.render(
   <Provider store={store}>
@@ -99,13 +108,21 @@ ReactDOM.render(
       <Route path="/" component={App} onEnter={appEnter}>
         <IndexRoute component={Home} />
         <Route path="/minions" component={AllMinions} />
-        <Route path="/minions/new" onEnter={newMinionEnter} components={Minion} />
-        <Route path="/minions/:id" onEnter={singleMinionEnter} components={Minion} />
+        <Route
+          path="/minions/new"
+          onEnter={newMinionEnter}
+          components={Minion}
+        />
+        <Route
+          path="/minions/:id"
+          onEnter={singleMinionEnter}
+          components={Minion}
+        />
         <Route path="/ideas" onEnter={allIdeasEnter} components={AllIdeas} />
-        <Route path="/ideas/new" onEnter={newIdeaEnter} components={Idea}/>
+        <Route path="/ideas/new" onEnter={newIdeaEnter} components={Idea} />
         <Route path="/ideas/:id" onEnter={singleIdeaEnter} components={Idea} />
       </Route>
     </Router>
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
--- browser/store/meetings.js
+++ browser/store/meetings.js
@@ -1,53 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
-const CREATE_MEETING = 'CREATE_MEETING';
-const CANCEL_MEETINGS = 'CANCEL_MEETINGS';
-const SET_MEETINGS = 'SET_MEETINGS';
+const CREATE_MEETING = "CREATE_MEETING";
+const CANCEL_MEETINGS = "CANCEL_MEETINGS";
+const SET_MEETINGS = "SET_MEETINGS";
 
-export const setMeetings = meetings => {
+export const setMeetings = (meetings) => {
   return {
     type: SET_MEETINGS,
     meetings,
-  }
-}
+  };
+};
 
-export const createMeeting = meeting => {
+export const createMeeting = (meeting) => {
   return {
     type: CREATE_MEETING,
     meeting,
-  }
-}
+  };
+};
 
 export const cancelMeetings = () => {
   return {
-    type: CANCEL_MEETINGS
-  }
-}
+    type: CANCEL_MEETINGS,
+  };
+};
 
-export const createMeetingThunk = () => dispatch => {
-  axios.post('http://localhost:4001/api/meetings')
-  .then(res => res.data)
-  .then(createdMeeting => {
-    dispatch(createMeeting(createdMeeting));
-  })
-  .catch(console.error.bind(console));
-}
+export const createMeetingThunk = () => (dispatch) => {
+  axios
+    .post("http://localhost:3000/api/meetings")
+    .then((res) => res.data)
+    .then((createdMeeting) => {
+      dispatch(createMeeting(createdMeeting));
+    })
+    .catch(console.error.bind(console));
+};
 
-export const cancelMeetingsThunk = () => dispatch => {
-  axios.delete('http://localhost:4001/api/meetings')
-  .then(() => {
-    dispatch(cancelMeetings());
-  })
-  .catch(console.error.bind(console));
-}
+export const cancelMeetingsThunk = () => (dispatch) => {
+  axios
+    .delete("http://localhost:3000/api/meetings")
+    .then(() => {
+      dispatch(cancelMeetings());
+    })
+    .catch(console.error.bind(console));
+};
 
 const initial = [];
 
 export default (initialState = initial, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case CREATE_MEETING:
       const newMeetings = [action.meeting, ...initialState];
-      newMeetings.sort((a, b) => { 
+      newMeetings.sort((a, b) => {
         return new Date(a.date).getTime() - new Date(b.date).getTime();
       });
       return newMeetings;
@@ -58,4 +60,4 @@ export default (initialState = initial, action) => {
     default:
       return initialState;
   }
-}
+};
